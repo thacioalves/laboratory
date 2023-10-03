6f51eb14867efccec97a8e228e05fbed9c388ea0
Thacio Alves <thaccio.alves@gmail.com>
2023-10-03 15:02:42 -0300

    login completed

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.messages import constants
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
 
 def cadastro(request):
     if request.method == "GET":
@@ -39,3 +40,17 @@ def cadastro(request):
 
         return redirect('/usuarios/cadastro')
 
+def logar(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        user = authenticate(username=username, password=senha)
+        if user:
+            login(request, user)
+            return redirect('/')
+        else:
+            messages.add_message(request, constants.ERROR, 'Username ou senha inválidos')
+            return redirect('/usuarios/login')
